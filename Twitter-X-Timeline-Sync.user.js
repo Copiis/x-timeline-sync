06ab58d8bf1d329444e2806ff2a788dc64e9d7b3
Copiis <copiis@vivaldi.net>
2026-06-20 15:37:32 +0200

    2026.6.20b: Spenden-URL auf GitHub Sponsors umgestellt
    Ersetzt Bitcoin/PayPal durch https://github.com/sponsors/Copiis

--- Twitter-X-Timeline-Sync.user.js
+++ Twitter-X-Timeline-Sync.user.js
@@ -14,7 +14,7 @@
 // @description:ko Twitter/X에서 마지막 읽기 위치를 추적하고 동기화합니다. 수동 및 자동 옵션 포함. 새로운 게시물을 확인하면서 현재 위치를 잃지 않도록 이상적입니다. 트윗 ID를 사용하여 정확한 위치 지정을 하고, 리포스트를 지원합니다。
 // @icon https://x.com/favicon.ico
 // @namespace https://github.com/Copiis/x-timeline-sync
-// @version 2026.6.20a
+// @version 2026.6.20b
 // @author Copiis
 // @license MIT
 // @match https://x.com/*
@@ -24,8 +24,7 @@
 // @downloadURL https://raw.githubusercontent.com/Copiis/x-timeline-sync/main/Twitter-X-Timeline-Sync.user.js
 // @updateURL https://raw.githubusercontent.com/Copiis/x-timeline-sync/main/Twitter-X-Timeline-Sync.user.js
 // @description If you find this script useful and would like to support my work, consider making a small donation!
-// @description Bitcoin (BTC): bc1quc5mkudlwwkktzhvzw5u2nruxyepef957p68r7
-// @description PayPal: https://www.paypal.com/paypalme/Coopiis?country.x=DE&locale.x=de_DE
+// @description GitHub Sponsors: https://github.com/sponsors/Copiis
 // ==/UserScript==
 //
 (function () {
